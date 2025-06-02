4081d027880ee428db747b0bbfc03147bee2b072
Chen Mingliang <illuspas@msn.com>
2025-06-02 20:57:46 +0800

    feat: 添加命令行参数支持和服务器启动功能
    - 在 package.json 中添加 bin 字段和 minimist 依赖
    - 实现命令行参数解析，支持配置证书文件、端口等参数
    - 添加 STUN 服务器和 WebSocket 信令服务器的启动逻辑
    - 包含默认的 SSL 证书和密钥文件

--- bin/app.js
+++ bin/app.js
@@ -1,4 +1,33 @@
 const MageSignaling = require("..");
+const path = require("path");
+const currentFilePath = path.resolve(__dirname);
+const argv = require("minimist")(process.argv.slice(2),
+  {
+    string: ["key_file", "cert_file", "stun_port", "signaling_port", "auth_key"],
+    alias: {
+      "key_file": "k",
+      "cert_file": "c",
+      "stun_port": "r",
+      "signaling_port": "s",
+      "auth_key": "a",
+    },
+    default: {
+      "key_file": path.join(currentFilePath, "key.pem"),
+      "cert_file": path.join(currentFilePath, "cert.pem"),
+      "stun_port": 19302,
+      "signaling_port": 8080,
+      "auth_key": "MageSignaling@2025"
+    }
+  });
 
-let ms = new MageSignaling();
+if (argv.help) {
+  console.log("Usage:");
+  console.log("  magesignaling --help // print help information");
+  console.log("  magesignaling --stun_port 19302 or -r 19302");
+  console.log("  magesignaling --signaling_port 8080 or -s 8080");
+  process.exit(0);
+}
+
+
+let ms = new MageSignaling(argv);
 ms.run();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
+const fs = require("fs");
+const ws = require("ws");
+const stun = require("stun");
+const https = require("https");
 
 class MageSignaling {
-    constructor() {
-    }
+  constructor(argv) {
+    this.argv = argv;
+  }
 
-    run() {
+  run() {
+    this.startSTUNServer();
+    this.startSignalingServer();
+  }
 
-    }
+  startSTUNServer() {
+    const server = stun.createServer({ type: "udp4" });
+
+    const { STUN_BINDING_RESPONSE, STUN_EVENT_BINDING_REQUEST } = stun.constants;
+    const userAgent = `node/${process.version} stun/v1.0.0`;
+
+    server.on(STUN_EVENT_BINDING_REQUEST, (request, rinfo) => {
+      const message = stun.createMessage(STUN_BINDING_RESPONSE, request.transactionId);
+
+      message.addXorAddress(rinfo.address, rinfo.port);
+      message.addSoftware(userAgent);
+
+      server.send(message, rinfo.port, rinfo.address);
+    });
+
+    server.listen(this.argv.stun_port, () => {
+      console.log("[stun] server started on port", this.argv.stun_port);
+    });
+  }
+
+  startSignalingServer() {
+    const server = https.createServer({
+      cert: fs.readFileSync(this.argv.cert_file),
+      key: fs.readFileSync(this.argv.key_file)
+    });
+
+    const wss = new ws.WebSocketServer({ server });
+
+    wss.on("connection", (ws) => {
+      ws.on("error", console.error);
+      ws.on("message", (data) => {
+        console.log("received: %s", data);
+      });
+    });
+
+    server.listen(this.argv.signaling_port, () => {
+      console.log("[signaling] server started on port", this.argv.signaling_port);
+    });
+  }
+
 }
 
 module.exports = MageSignaling;
